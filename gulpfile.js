535ae296185dc22d793a77ef8f06195764c6cb49
Pedro aguilar barbaran <hgrlatam@outlook.es>
2021-10-27 18:33:32 -0500

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
-const {src,dest,watch} = require('gulp');
+const {src,dest,watch,parallel} = require('gulp');
+
+// CSS
 const sass = require('gulp-sass') (require('sass')) ;
-const plumber =require('gulp-plumber')
+const plumber =require('gulp-plumber');
+
+//IMG
+const webp = require('gulp-webp');
 
 function css( done ){
     src('src/scss/**/*.scss')// Identificar el archivo .scss a compilar
         .pipe(plumber())
         .pipe(sass()) //Compilarlo
         .pipe(dest('build/css'))//Almancenar
+    done();
+}
+function versionWebp (done){
+
+    const opciones = { 
+        quality: 50
+    };
+
+    src('src/img/**/*.{png.jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+
     done();
 }
 
@@ -16,4 +33,5 @@ function dev ( done ){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp=versionWebp;
+exports.dev = parallel(versionWebp,dev);
