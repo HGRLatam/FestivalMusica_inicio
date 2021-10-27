2d54cec9a9eeae18b814a11a6c7450b1a23d21ee
Pedro aguilar barbaran <hgrlatam@outlook.es>
2021-10-27 18:36:42 -0500

    pocos cambios

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,14 @@ function css( done ){
         .pipe(dest('build/css'))//Almancenar
     done();
 }
+
 function versionWebp (done){
 
     const opciones = { 
         quality: 50
     };
 
-    src('src/img/**/*.{png.jpg}')
+    src('src/img/**/*.{png,jpg}')
     .pipe(webp(opciones))
     .pipe(dest('build/img'))
 
