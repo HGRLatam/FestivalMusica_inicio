4def14529f44be23edf0cac92f963ce8efb6ddce
pedro aguilar barbaran <hgrlatam@outlook.es>
2021-11-07 14:27:10 -0500

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const {src,dest,watch,parallel} = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
 // CSS
-const sass = require('gulp-sass') (require('sass')) ;
-const plumber =require('gulp-plumber');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 //IMG
 const webp = require('gulp-webp');
 
-function css( done ){
+function css(done) {
     src('src/scss/**/*.scss')// Identificar el archivo .scss a compilar
         .pipe(plumber())
         .pipe(sass()) //Compilarlo
@@ -15,24 +15,21 @@ function css( done ){
     done();
 }
 
-function versionWebp (done){
-
-    const opciones = { 
+function versionWebp(done) {
+    const opciones = {
         quality: 50
     };
-
     src('src/img/**/*.{png,jpg}')
-    .pipe(webp(opciones))
-    .pipe(dest('build/img'))
-
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
     done();
 }
 
-function dev ( done ){
+function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.versionWebp=versionWebp;
-exports.dev = parallel(versionWebp,dev);
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
