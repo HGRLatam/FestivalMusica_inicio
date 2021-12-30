61b67b7de8053d0cf480b29a822e4619111c2796
Pedro <hgrlatam@gmail.com>
2021-12-30 14:11:16 -0500

    deployment

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const { src, dest, watch, parallel } = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');  
+const postcss =require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //IMG
 const cache = require('gulp-cache');
@@ -10,10 +14,16 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     src('src/scss/**/*.scss')// Identificar el archivo .scss a compilar
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) //Compilarlo
+        .pipe(postcss([autoprefixer(),cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'))//Almancenar
     done();
 }
@@ -47,6 +57,9 @@ function versionWebp(done) {
 }
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write())
         .pipe(dest('build/js'));
     done();
 }
