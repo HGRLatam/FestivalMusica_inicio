2543ea91029510814487bb893f0b14d3bcdef667
Pedro <hgrlatam@gmail.com>
2021-12-30 13:27:26 -0500

    nuevos cambios

--- build/js/app.js
+++ build/js/app.js
@@ -3,9 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+    
+    navegacionFija();
     crearGaleria();
+    scrollNav();
 }
+function navegacionFija(){
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
 
+    window.addEventListener('scroll',function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 0){
+            barra.classList.add('fijo');
+        }else{
+            barra.classList.remove('fijo');
+        }
+    });
+}
+        
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener ('click', function(e){
+            e.preventDefault();
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelectorAll(seccionScroll);
+            seccion.seccionIntoView();
+        });
+    })
+}
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
